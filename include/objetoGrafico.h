f86f55fbc930bfad059f5b0e48d0338185009910
kawmarco <kawajirimarco@googlemail.com>
2012-05-29 23:01:05 -0300

    Implementação rudimentar do desenhaTriangulo()

--- include/objetoGrafico.h
+++ include/objetoGrafico.h
@@ -8,6 +8,7 @@
 #define TCIRCULO   'C'
 #define TESFERA    'E'
 #define TRETANGULO 'R'
+#define TTRIANGULO 'T'
 #define TCUBO      'Q'
 #define TCONE      'O'
 #define TTORUS     'U'
@@ -18,17 +19,19 @@ typedef struct{
     float valores[MAX_VALORES]; // R;G;B;Espessura;Tipografia;Orientacao[3];Translacao[3];p1x;p1y;p1z;p2x;p2y;p3z;...
 } objetoGrafico;                //Espessura == 0 -> solido
 
-static float* getCor(objetoGrafico* og)       { return &og->valores[0]; }
-static float  getEspessura(objetoGrafico* og) { return  og->valores[3]; }
-static float  getTipografia(objetoGrafico* og){ return  og->valores[4]; }
-static float* getOrientacao(objetoGrafico* og){ return &og->valores[5]; }
-static float* getTranslacao(objetoGrafico* og){ return &og->valores[8]; } 
+static float* getCor(objetoGrafico* og)         { return &og->valores[0]; }
+static float  getEspessura(objetoGrafico* og)   { return  og->valores[3]; }
+static float  getTipografia(objetoGrafico* og)  { return  og->valores[4]; }
+static float* getOrientacao(objetoGrafico* og)  { return &og->valores[5]; }
+static float* getTranslacao(objetoGrafico* og)  { return &og->valores[8]; } 
+static float* getValoresExtra(objetoGrafico* og){ return &og->valores[11];}
 
 void desenhaPonto(objetoGrafico* og);
 void desenhaLinha(objetoGrafico* og);
 void desenhaCirculo(objetoGrafico* og);
 void desenhaEsfera(objetoGrafico* og);
 void desenhaRetangulo(objetoGrafico* og);
+void desenhaTriangulo(objetoGrafico* og);
 void desenhaCubo(objetoGrafico* og);
 void desenhaCone(objetoGrafico* og);
 void desenhaTorus(objetoGrafico* og);
@@ -39,7 +42,8 @@ static int numParametros(char tipo){
 		case TLINHA:     return -1; 
 		case TCIRCULO:   return -1; 
 		case TESFERA:    return -1; 
-		case TRETANGULO: return -1; 
+		case TRETANGULO: return -1;
+		case TTRIANGULO: return 3+1+1+3+3+3*3; //3cor + 1epessura + 1tipografia + 3orientacao + 3translacao + 3xyz*3
 		case TCUBO:      return -1; 
 		case TCONE:      return -1; 
 		case TTORUS:     return -1; 
@@ -54,11 +58,12 @@ static void desenhaObjetoGrafico(objetoGrafico* og){
 		case TCIRCULO:   return desenhaCirculo(og); 
 		case TESFERA:    return desenhaEsfera(og); 
 		case TRETANGULO: return desenhaRetangulo(og); 
+		case TTRIANGULO: return desenhaTriangulo(og);
 		case TCUBO:      return desenhaCubo(og); 
 		case TCONE:      return desenhaCone(og); 
 		case TTORUS:     return desenhaTorus(og); 
     }
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
 
 
--- objetoGrafico.c
+++ objetoGrafico.c
@@ -6,33 +6,49 @@
 #include <gl/glu.h>			//Util library
 
 void desenhaPonto(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
 
 void desenhaLinha(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
 
 void desenhaCirculo(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
 
 void desenhaEsfera(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
 
 void desenhaRetangulo(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
+}
+
+void desenhaTriangulo(objetoGrafico* og){
+     float* cor = getCor(og);
+     float* verts[3];
+     int i;
+     for(i=0;i<3;i++){
+        verts[i] = &getValoresExtra(og)[i*3];
+     }
+     glBegin(GL_TRIANGLES);
+         glColor3f(cor[0],cor[1],cor[2]);
+         for(i=0;i<3;i++){
+             float* v = verts[i];
+             glVertex3f(v[0],v[1],v[2]);
+         }
+     glEnd();
 }
 
 void desenhaCubo(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
 
 void desenhaCone(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
 
 void desenhaTorus(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(%c)", og->tipo);
+    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
 }
