430b565fcb7e4c09fd6ade42771253312bdd5797
kawmarco <kawajirimarco@googlemail.com>
2012-06-07 05:27:38 -0300

    Impl. desenhaTriangulo() e desenhaLinha()
    -Introdução da macro #DEBUG_SPIN_SPIN (tente! É divertido)
    -Implementação do desenhaTriangulo()
    -Implementação do desenhaLinha()
    -Definições explicitas de tamanhos e indices de argumentos
    (objetoGrafico.h)
    -Introdução de aplicaCorTransformacoesPadrao() (objetoGrafico.c)

--- include/objetoGrafico.h
+++ include/objetoGrafico.h
@@ -3,6 +3,8 @@
 
 #define MAX_VALORES 50
 
+#define NPONTO 3
+
 #define TPONTO     'P'
 #define TLINHA     'L'
 #define TCIRCULO   'C'
@@ -13,18 +15,36 @@
 #define TCONE      'O'
 #define TTORUS     'U'
 
+// Numero de argumentos padrao
+#define NCOR        3
+#define NESPESSURA  1
+#define NTIPOGRAFIA 1
+#define NORIENTACAO 4
+#define NTRANSLACAO 3
+#define NARGSCOMUNS (NCOR+NESPESSURA+NTIPOGRAFIA+NORIENTACAO+NTRANSLACAO)
+
+// Indice dos argumentos
+#define ICOR 0
+#define IESPESSURA  (ICOR+NCOR)
+#define ITIPOGRAFIA (IESPESSURA+NESPESSURA)
+#define IORIENTACAO (ITIPOGRAFIA+NTIPOGRAFIA)
+#define ITRANSLACAO (IORIENTACAO+NORIENTACAO)
+#define IRESTO      (ITRANSLACAO+NTRANSLACAO)
+
 typedef struct{
     int id;
     char tipo;
-    float valores[MAX_VALORES]; // R;G;B;Espessura;Tipografia;Orientacao[3];Translacao[3];p1x;p1y;p1z;p2x;p2y;p3z;...
+    float valores[MAX_VALORES]; // R;G;B;Espessura;Tipografia;Orientacao[4];Translacao[3];p1x;p1y;p1z;p2x;p2y;p3z;...
 } objetoGrafico;                //Espessura == 0 -> solido
 
-static float* getCor(objetoGrafico* og)         { return &og->valores[0]; }
-static float  getEspessura(objetoGrafico* og)   { return  og->valores[3]; }
-static float  getTipografia(objetoGrafico* og)  { return  og->valores[4]; }
-static float* getOrientacao(objetoGrafico* og)  { return &og->valores[5]; }
-static float* getTranslacao(objetoGrafico* og)  { return &og->valores[8]; } 
-static float* getValoresExtra(objetoGrafico* og){ return &og->valores[11];}
+static float* getCor(objetoGrafico* og)         { return &og->valores[ICOR]; }
+static float  getEspessura(objetoGrafico* og)   { return  og->valores[IESPESSURA]; }
+static float  getTipografia(objetoGrafico* og)  { return  og->valores[ITIPOGRAFIA]; }
+static float* getOrientacao(objetoGrafico* og)  { return &og->valores[IORIENTACAO]; }
+static float* getTranslacao(objetoGrafico* og)  { return &og->valores[ITRANSLACAO]; } 
+static float* getValoresExtra(objetoGrafico* og){ return &og->valores[IRESTO];}
+
+static float  ehSolido(objetoGrafico* og)       { return  og->valores[IESPESSURA] == 0; }
 
 void desenhaPonto(objetoGrafico* og);
 void desenhaLinha(objetoGrafico* og);
@@ -38,12 +58,12 @@ void desenhaTorus(objetoGrafico* og);
 
 static int numParametros(char tipo){
     switch(tipo){ //XXX preencher numero de parametros correto
-        case TPONTO:     return 3+1+3; //3cor + 1espessura + 1xyz?
-		case TLINHA:     return -1; 
+        case TPONTO:     return NCOR+NESPESSURA+1*NPONTO; //3cor + 1espessura + 1xyz?
+		case TLINHA:     return NARGSCOMUNS+2*NPONTO; 
 		case TCIRCULO:   return -1; 
 		case TESFERA:    return -1; 
 		case TRETANGULO: return -1;
-		case TTRIANGULO: return 3+1+1+3+3+3*3; //3cor + 1epessura + 1tipografia + 3orientacao + 3translacao + 3xyz
+		case TTRIANGULO: return NARGSCOMUNS+3*NPONTO; //3cor + 1epessura + 1tipografia + 3orientacao + 3translacao + 3xyz
 		case TCUBO:      return -1; 
 		case TCONE:      return -1; 
 		case TTORUS:     return -1; 
@@ -52,7 +72,7 @@ static int numParametros(char tipo){
 }
 
 static void desenhaObjetoGrafico(objetoGrafico* og){
-    switch(og->tipo){ //XXX preencher numero de parametros correto
+    switch(og->tipo){
         case TPONTO:     return desenhaPonto(og); 
 		case TLINHA:     return desenhaLinha(og); 
 		case TCIRCULO:   return desenhaCirculo(og); 
--- objetoGrafico.c
+++ objetoGrafico.c
@@ -9,6 +9,31 @@
 void printErro(objetoGrafico* og, char* erro)  { printf("ERRO(id=%d, tipo=%c): %s\n", og->id, og->tipo, erro); }
 void printAviso(objetoGrafico* og, char* erro){ printf("AVISO(id=%d, tipo=%c): %s\n", og->id, og->tipo, erro); }
 
+void DEBUG_printObj(objetoGrafico* og){ 
+     printf("objetoGrafico<id=%d, tipo=%c, args={", og->id, og->tipo);
+     int i;
+     for(i=0;i<numParametros(og->tipo);i++){
+         printf("%f, ",og->valores[i]);
+     }
+     printf("}>\n");
+}
+
+void aplicaCorTransformacoesPadrao(objetoGrafico* og){
+     float* cor          = getCor(og);
+     float* orient       = getOrientacao(og);
+     float* trans        = getTranslacao(og);
+     float  espessura    = getEspessura(og);
+     GLushort tipografia = getTipografia(og);
+
+     //Cor e transformacoes
+     glColor3f(cor[0],cor[1],cor[2]);
+     glTranslatef(trans[0],trans[1],trans[2]);
+     glRotatef(orient[0],orient[1],orient[2],orient[3]);
+
+     //Configuracoes de linha
+     glLineStipple(1, tipografia);
+     glLineWidth(espessura);
+}
 
 //Funcoes de desenho
 void desenhaPonto(objetoGrafico* og){
@@ -16,7 +41,23 @@ void desenhaPonto(objetoGrafico* og){
 }
 
 void desenhaLinha(objetoGrafico* og){
-    printf("ERRO: Tipo nao implementado(tipo=%c,id=%d)\n", og->tipo, og->id);
+     glPushMatrix(); //Salva a matriz    
+     aplicaCorTransformacoesPadrao(og);
+
+     int i;
+     float* verts[2];
+     for(i=0;i<2;i++){
+        verts[i] = &getValoresExtra(og)[i*3];
+     }
+
+     glBegin(GL_LINES);
+         for(i=0;i<2;i++){
+             float* v = verts[i];
+             glVertex3f(v[0],v[1],v[2]);
+         }
+     glEnd();
+
+     glPopMatrix(); //Deixa a matriz como ela estava antes
 }
 
 void desenhaCirculo(objetoGrafico* og){
@@ -32,20 +73,26 @@ void desenhaRetangulo(objetoGrafico* og){
 }
 
 void desenhaTriangulo(objetoGrafico* og){
-     printAviso(og, "Desconsiderando translacao, orientacao, preenchimento e tipografia (nao implementado)"); //XXX aprender a usar a pilha de matrizes
-     float* cor = getCor(og);
-     float* verts[3];
+     glPushMatrix(); //Salva a matriz
+     aplicaCorTransformacoesPadrao(og);     
+
      int i;
+     float* verts[3];
      for(i=0;i<3;i++){
         verts[i] = &getValoresExtra(og)[i*3];
      }
-     glBegin(GL_TRIANGLES);
-         glColor3f(cor[0],cor[1],cor[2]);
+
+     GLenum modoDesenho;
+     if(ehSolido(og)) modoDesenho = GL_TRIANGLES;
+     else             modoDesenho = GL_LINE_LOOP;
+     glBegin(modoDesenho);
          for(i=0;i<3;i++){
              float* v = verts[i];
              glVertex3f(v[0],v[1],v[2]);
          }
      glEnd();
+     
+     glPopMatrix(); //Deixa a matriz como ela estava antes
 }
 
 void desenhaCubo(objetoGrafico* og){
