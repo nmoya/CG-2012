aaf57067542aaafef58982473b33fb5607a826a8
Nikolas Moya <nikolasmoya@gmail.com>
2012-06-21 03:29:23 -0300

    Orbita e Zoom

--- mainCG.c
+++ mainCG.c
@@ -6,10 +6,10 @@
 #include <gl/glut.h>
 #include <gl/glu.h>			//Util library
 
-#define DEBUG_SPIN_SPIN //SPIN, SPIN! Se definido, saia do programa com um CTRL+C na janela de linha de comando
+//#define DEBUG_SPIN_SPIN //SPIN, SPIN! Se definido, saia do programa com um CTRL+C na janela de linha de comando
 
 GLfloat nRange = 50.0f;  //XXX acredito que o professor tenha dito que essa variavel tenha que ser ajustada automaticamente (para caber toda a cena?)
-
+int height, width;
 objetoGrafico* objetosGraficos;
 int objetosGraficos_len;
 
@@ -64,12 +64,14 @@ void Inicializa (void)
 // Tipografia de linha
    glEnable(GL_LINE_STIPPLE);
 
-// Normaliza normais automaticamente
-   glEnable(GL_NORMALIZE);
+
+
 }
 
 void reshape(int w, int h)
 {
+     height = h;
+     width = w;
  // glMatrixMode(GL_PROJECTION);  /* Start modifying the projection matrix. */
   	// Prevent a divide by zero
 	if(h == 0)
@@ -84,6 +86,53 @@ void reshape(int w, int h)
     else 
 		glOrtho (-nRange*w/h, nRange*w/h, -nRange, nRange, -nRange, nRange);
 }
+// Função callback chamada para gerenciar eventos de teclas
+void Teclado (unsigned char key, int x, int y)
+{
+             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        int i;
+        for(i=0; i<objetosGraficos_len; i++){
+            desenhaObjetoGrafico(&objetosGraficos[i]);
+        }
+        glFlush();
+        glutSwapBuffers();
+	if (key == 27)    //ESC
+		exit(0);
+	if (key == 'w')
+	{
+	   glRotatef(5,1,0,0);
+    }
+    else if (key == 'a')
+    {         
+        glRotatef(-5,0,1,0); 
+    }
+    else if (key == 's')
+    {
+         glRotatef(-5,1,0,0);
+    }
+    else if (key == 'd')
+    {
+         glRotatef(5,0,1,0); 
+    }
+    else if (key == 'z')     //Zoom In
+    {
+         nRange+=10;
+         reshape(width, height);
+    }
+    else if (key == 'x')     //Zoom 'Out'
+    {
+         nRange-=10;
+         reshape(width, height);
+    }
+    else if (key == 'i')
+    {
+    }
+    else if (key == 'v')
+    {
+          
+    }
+}
+
 // Programa Principal 
 int main(void)
 {
@@ -101,6 +150,8 @@ int main(void)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutCreateWindow("Visualizador");
 	glutDisplayFunc(Desenha);
+    // Registra a função callback para tratamento das teclas ASCII
+	glutKeyboardFunc (Teclado);
 	Inicializa();
     glutReshapeFunc(reshape);
 	glutMainLoop();
@@ -108,3 +159,7 @@ int main(void)
 	free(objetosGraficos);
 	return 0;
 }
+
+
+
+
